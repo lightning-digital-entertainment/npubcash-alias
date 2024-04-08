487e6d8598a409536d2a61fb1ba76acaac22d079
Egge <egge@getcurrent.io>
2024-04-08 15:43:10 +0200

    updated model

--- src/models/PaymentRequest.ts
+++ src/models/PaymentRequest.ts
@@ -5,30 +5,61 @@ export class PaymentRequest {
   created_at: number;
   pubkey: string;
   payment_request: string;
+  payment_hash: string;
   alias: string;
   status: "pending" | "cancelled" | "done";
+  amount: number;
 
   constructor() {}
 
   static async createPaymentRequest(
     pubkey: string,
     payment_request: string,
+    payment_hash: string,
     alias: string,
+    amount: number,
   ) {
-    const query = `INSERT INTO l_alias_requests (pubkey, payment_request, alias, status) VALUES ($1, $2, $3, $4)`;
-    const params = [pubkey, payment_request, alias, "pending"];
+    const query = `INSERT INTO l_alias_requests (pubkey, payment_request, payment_hash, alias, status, amount) VALUES ($1, $2, $3, $4, $5, $6)`;
+    const params = [
+      pubkey,
+      payment_request,
+      payment_hash,
+      alias,
+      "pending",
+      amount,
+    ];
     const queryRes = await queryWrapper(query, params);
     if (queryRes.rowCount === 0) {
       throw new Error("did not create payment request");
     }
   }
 
+  static async getPaymentRequestByHash(payment_hash: string) {
+    const query = `SELECT * from l_alias_requests WHERE payment_hash = $1`;
+    const params = [payment_hash];
+    const queryRes = await queryWrapper<PaymentRequest>(query, params);
+    return queryRes.rows[0];
+  }
+
+  static async cancelTimedOutRequests() {
+    const query = `UPDATE l_alias_requests SET status 'cancelled' WHERE created_at < now() - interval '7minutes'`;
+    const res = await queryWrapper(query, []);
+    console.log(`Updated ${res.rowCount} rows...`);
+  }
+
+  static async checkIfReserved(alias: string) {
+    const query = `SELECT * FROM l_alias_requests WHERE alias = $1 AND status = 'pending'`;
+    const params = [alias];
+    const queryRes = await queryWrapper(query, params);
+    return queryRes.rows.length > 0;
+  }
+
   static async updatePaymentRequestStatus(
     status: "done" | "cancelled",
-    payment_request: string,
+    payment_hash: string,
   ) {
-    const query = `UPDATE l_alias_requests SET status = $1 WHERE payment_request = $2`;
-    const params = [status, payment_request];
+    const query = `UPDATE l_alias_requests SET status = $1 WHERE payment_hash = $2`;
+    const params = [status, payment_hash];
     const queryRes = await queryWrapper(query, params);
     if (queryRes.rowCount === 0) {
       throw new Error("did not update payment request");
